ade85f04be5f523b577213e61424297aaecea1db
lucasswm <100297769+lucasswm@users.noreply.github.com>
2022-02-23 14:26:32 -0300

    remoção 5min

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Respostas - 5min ⏱️
+            Respostas
           </Link>
         </div>
       </div>
